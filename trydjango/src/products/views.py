c77ab500869ed767ba13fd5d25013a63a47dd34d
kmcodes <6884180+kmcodes@users.noreply.github.com>
2019-02-22 20:09:27 +0530

    changes till forms

--- trydjango/src/products/views.py
+++ trydjango/src/products/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     context = {
         "objects":obj
     }
-    return render(request, "product/detail.html",context)
+    return render(request, "products/product_detail.html",context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#     context = {
+#         'form':form
+
+#     }    
+
+#     return render(request,"products/product_create.html",context)
+
+def product_create_view(request):
+    context = {}
+    return render(request,'products/product_create.html', context)
